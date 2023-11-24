31d5f9741bcc924b40e6cf325164da6498d8bc39
javier <kuznikjavier@gmail.com>
2023-11-23 22:10:41 -0300

    subir al repo la estructura del back

---
+++ ecommers/src/main/java/com/example/ecommers/EcommersApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommersApplication.class, args);
+	}
+
+}
