21140fdf8efc4492e3b31a93e8f30c06610b64af
Alan <alan.papillu@hotmail.com>
2023-11-25 11:04:30 -0300

    Update ProductoEntity class

--- ecommers/src/main/java/com/example/ecommers/model/ProductoEntity.java
+++ ecommers/src/main/java/com/example/ecommers/model/ProductoEntity.java
@@ -14,18 +14,24 @@ public class ProductoEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idProducto;
 
+    
+    //nombre del producto
     @Column(length = 100, nullable = false)
     private String productoNombre;
 
+    // Cantidad disponible en el inventario
     @Column(nullable = false)
     private int productoCantidad;
 
+    // Imagen asociada al producto
     @Column(length = 200)
     private String productoImagen;
 
+    // Precio del producto
     @Column(nullable = false)
     private double productoPrecio;
 
+    // Categoría del producto (relación Many-to-One con CategoryEntity)
     @ManyToOne
     @JoinColumn(name = "categoria_id", nullable = false)
     private CategoryEntity tipoCategoria;
