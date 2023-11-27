0d4c29c6f326bd07b0d686e262f8a3ede4c940f8
Juan Insua <insuajuancruz@gmail.com>
2023-11-26 22:04:51 -0300

    Add Cors Config
    @CrossOrigin(origins = "http://localhost:5173")

--- ecommers/src/main/java/com/example/ecommers/controller/CategoryController.java
+++ ecommers/src/main/java/com/example/ecommers/controller/CategoryController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/category")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CategoryController {
 
     private final I_CategoryService categoryService;
--- ecommers/src/main/java/com/example/ecommers/controller/ProductController.java
+++ ecommers/src/main/java/com/example/ecommers/controller/ProductController.java
@@ -10,17 +10,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/v1/product")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
     
     @Autowired
