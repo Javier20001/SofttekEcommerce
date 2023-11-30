6fc0e797f3ee05206a561fc58f36b85764aa92ab
Alan Papillu <alan.papillu@hotmail.com>
2023-11-30 14:32:52 -0300

    Create Role Entity

---
+++ ecommers/src/main/java/com/example/ecommers/model/RoleEntity.java
@@ -0,0 +1,30 @@
+package com.example.ecommers.model;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+
+
+/**
+ * Entidad que representa los roles de usuarios en el sistema.
+ */
+@AllArgsConstructor 
+@NoArgsConstructor
+@Entity
+@Data
+public class RoleEntity {
+    
+    // Identificador único del rol
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Nombre del rol, utilizado como un Enum para definir roles predefinidos
+    @Enumerated(EnumType.STRING)
+    private RolesName name;
+
+    // Relación Many-to-Many con la entidad User, mapeada por el atributo 'roles' en User
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+}
