32b0620b4f7548394dc2b15edebf2d634a3b1b23
CM-Gimenez <martingimenez2222@gmail.com>
2023-12-04 08:35:54 -0300

    validacion de datos de usuario en la entidad usuario y en el DTO del login

--- ecommers/src/main/java/com/example/ecommers/model/UserEntity.java
+++ ecommers/src/main/java/com/example/ecommers/model/UserEntity.java
@@ -1,6 +1,9 @@
 package com.example.ecommers.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,29 +19,40 @@ import java.util.List;
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     // Nickname del usuario (único y no nulo)
     @Column(length = 20, unique = true, nullable = false)
+    @NotBlank(message = "User is required")
     private String userName;
 
     // Dirección de correo electrónico del usuario (único y no nulo)
     @Column(length = 40, unique = true, nullable = false)
+    @Email(message = "Must be a valid email address")
+    @NotBlank(message = "Email is required")
     private String email;
 
     // Contraseña del usuario (no nula)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     @Column(length = 60, nullable = false)
     private String password;
+
     //token de reseteo
     @Column
     private String resetToken;
+
     //duracion del token
     @Column
     private LocalDateTime expirationDate;
+
     // Roles asociados al usuario (relación Many-to-Many con Rol)
     @ManyToMany(fetch = FetchType.EAGER)
+    @NotBlank
     private List<RoleEntity> roles;
     
     @Column(columnDefinition = "boolean default true")
+    @NotBlank
     private Boolean status;
 }
