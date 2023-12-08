a37ea7cbc17d2438dbc01d0524b6ef62935caaa8
luciano <luciano.marti12@hotmail.com>
2023-12-08 00:20:36 -0300

    Repository

--- ecommers/src/main/java/com/example/ecommers/repository/I_ItemBidRepository.java
+++ ecommers/src/main/java/com/example/ecommers/repository/I_ItemBidRepository.java
@@ -3,6 +3,8 @@ package com.example.ecommers.repository;
 
 import com.example.ecommers.model.ItemBiEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface I_ItemBidRepository extends JpaRepository<ItemBiEntity, Long> {
 }
